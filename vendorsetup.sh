81b5342f3af5aa095771e6f9e3c99c7cdc8fab11
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-01-09 07:40:45 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,77 +18,51 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="a71"
-#set -o xtrace
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
-
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
-
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="a71n,sm-a715f"
-	export OF_TARGET_DEVICES="a71,a71n,sm-a715f"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_TWRP_COMPATIBILITY_MODE=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-        export OF_QUICK_BACKUP_LIST="/recovery;/boot;/data;"
-	export OF_PATCH_AVB20=1
-        export FOX_DELETE_AROMAFM=1
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-        export FOX_ENABLE_APP_MANAGER=1
-
-        # OTA
-        export OF_KEEP_DM_VERITY=1
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-	# Screen Settings
-	export OF_SCREEN_H=2280
-	export OF_STATUS_H=81
-	export OF_STATUS_INDENT_LEFT=52
-	export OF_STATUS_INDENT_RIGHT=52
-	export OF_CLOCK_POS=1
-	export OF_HIDE_NOTCH=1
-	export OF_ALLOW_DISABLE_NAVBAR=1
+export TW_DEFAULT_LANGUAGE="en"
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_DEVICE_ALT="a71n"	
+export OF_TARGET_DEVICES="a71,a71n"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+export OF_USE_GREEN_LED=0
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_TWRP_COMPATIBILITY_MODE=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export OF_QUICK_BACKUP_LIST="/recovery;/boot;/data;"
+export OF_PATCH_AVB20=1
+export FOX_DELETE_AROMAFM=1
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+export FOX_ENABLE_APP_MANAGER=1
+export OF_ENABLE_LPTOOLS=1
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_NEW_MAGISKBOOT_FORCE_AVB_VERIFY=1
+export FOX_VARIANT="Android12"
+# Screen Settings
+export OF_SCREEN_H=2280
+export OF_STATUS_H=81
+export OF_STATUS_INDENT_LEFT=52
+export OF_STATUS_INDENT_RIGHT=52
+export OF_CLOCK_POS=1
+export OF_HIDE_NOTCH=1
+export OF_ALLOW_DISABLE_NAVBAR=1
 	
-	# R11.1 Settings
-	export FOX_VERSION="R11.1_2"
-	export OF_MAINTAINER="Geekmaster21"
-	export OF_MAINTAINER_AVATAR="/home/ubuntu/fox_11.0/1.png"
+# R11.1 Settings
+export FOX_VERSION="R11.1_2"
+export OF_MAINTAINER="Geekmaster21"
+export OF_MAINTAINER_AVATAR="/home/ubuntu/fox_11.0/1.png"
 
 	# Run a Process after Formatting Data to Recreate /data/media/0 (only when forced-encryption is being disabled)
-	export OF_RUN_POST_FORMAT_PROCESS=1
-
-	# Let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-fi
-#
+export OF_RUN_POST_FORMAT_PROCESS=1
